d1053cfcc9d4feb464658c5b89d2c954cc6cdc99
Kazi Md. Tawsif Rahman <tawsifcse113@gmail.com>
2025-06-28 15:56:07 +0600

    config cors

--- application/src/main/java/com/crm/config/WebConfig.java
+++ application/src/main/java/com/crm/config/WebConfig.java
@@ -1,28 +1,17 @@
 package com.crm.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Allow all endpoints
-                        .allowedOrigins(
-                                "https://crm-frontend-taupe.vercel.app",
-                                "https://crm-production-747d.up.railway.app",
-                                "http://localhost:3000"
-                        )
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true); // If using cookies or auth headers
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Applies to all endpoints
+                .allowedOrigins("https://crm-production-747d.up.railway.app","https://crm-frontend-taupe.vercel.app")  // Swagger UI domain
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }

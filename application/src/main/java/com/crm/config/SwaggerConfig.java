3a90672f907ca382be30f3cef1514f823671f0a8
Kazi Md. Tawsif Rahman <tawsifcse113@gmail.com>
2025-06-28 17:33:49 +0600

    config cors

---
+++ application/src/main/java/com/crm/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.crm.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://crm-production-747d.up.railway.app"));
+    }
+}
